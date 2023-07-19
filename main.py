e745e8594e7237e6bdbf1c654c38f162422f9d6b
Amicht <vicolicht@gmail.com>
2023-07-19 05:30:49 +0300

    project final updates

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from product import ProductTracker
+from product_tracker import ProductTracker
 
 
 # product config
-name = "LED Projector"
+product_name = "LED Projector"
 product_url = "https://www.amazon.com/-/he/%D7%95%D7%91%D7%9C%D7%95%D7%98%D7%95%D7%AA-%D7%97%D7%99%D7%A6%D7%95" \
    "%D7%A0%D7%99-13000L-V6-Keystone/dp/B08HLLQV1J/ref=sr_1_3?crid=3C7L4PB91XH4C&keywords=LED" \
    "+Projector&qid=1689721592&sprefix=led+projector%2Caps%2C205&sr=8-3"
@@ -10,7 +10,7 @@ min_price = 129
 
 
 # start daily price check
-product = ProductTracker(url=product_url, name=name, min_price=min_price)
-product.start_price_check()
+product_tracker = ProductTracker(url=product_url, name=product_name, min_price=min_price)
+product_tracker.start_price_check()
 
 
--- readme.md
+++ readme.md
@@ -1,8 +1,18 @@
 ## Amazon Price Tracker
 
 ### Description
-The Amazon Price Tracker Check a specific product in Amazon every day and alert the user by email, if the product's price is under the requsted value.
+The Amazon Price Tracker Check a specific product in Amazon every day and alert the user by email, if the product's price is under the requested value.
 
+
+### Instructions
+In The 'main.py' file, set your own configuration:
+
+    product_name, 
+    min_price: the lowest price value in Dollars you are willing to pay,
+    product_url: a link to the product in Amazon
+
+And that is it!
+The APT will take care of the rest.
 
 ### Resources And Libraries
 - requests: server request
